86b007a9932a0a4252e1c1d50dbac4dbe1be06e1
rakesh b <rakeshbsirvi46@gmail.com>
2025-06-10 23:10:24 +0530

    command to signal the pg rest to start again

--- post-migrate.sh
+++ post-migrate.sh
@@ -1,3 +1,3 @@
 echo "Running post-migrate.sh"
-docker ps --filter name=_db_ --format '{{.Names}}'
-docker kill -s SIGUSR1 $(docker ps --filter name=_db_ --format '{{.Names}}')
+docker ps --filter name=_postgrest_ --format '{{.Names}}'
+docker kill -s SIGUSR1 $(docker ps --filter name=_postgrest_ --format '{{.Names}}')
