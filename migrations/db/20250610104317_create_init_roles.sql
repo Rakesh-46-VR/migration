519049edc2141e3554913072972f73c741cdd990
rakesh b <rakeshbsirvi46@gmail.com>
2025-06-10 19:05:02 +0530

    debug : changed to simple queries

--- migrations/db/20250610104317_create_init_roles.sql
+++ migrations/db/20250610104317_create_init_roles.sql
@@ -1,11 +1,5 @@
 ﻿-- migrate:up
 CREATE ROLE IF NOT EXISTS anon NOLOGIN;
-CREATE ROLE IF NOT EXISTS authenticated NOLOGIN;
-CREATE ROLE IF NOT EXISTS service_role NOLOGIN;
-CREATE ROLE IF NOT EXISTS supabase_auth_admin LOGIN PASSWORD 'SuperSecureAdminPassword';
 
 -- migrate:down
 DROP ROLE IF EXISTS service_role;
-DROP ROLE IF EXISTS authenticated;
-DROP ROLE IF EXISTS anon;
-DROP ROLE IF EXISTS supabase_auth_admin;
