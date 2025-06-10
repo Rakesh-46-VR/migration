b38157b79e682fe697e08597c4266a8edb950b4c
rakesh b <rakeshbsirvi46@gmail.com>
2025-06-10 23:25:33 +0530

    signal the auth service or any other service to start

--- post-migrate.sh
+++ post-migrate.sh
@@ -1,3 +1,4 @@
 echo "Running post-migrate.sh"
 docker ps --filter name=_postgrest_ --format '{{.Names}}'
-docker kill -s SIGUSR1 $(docker ps --filter name=_postgrest_ --format '{{.Names}}')
+docker kill -s SIGUSR1 docker-postgrest-1
+touch /sync/migrations-done
